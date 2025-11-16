534aaa286d500031cb67ba6ce074815778e809ef
Shashank433 <vanukuri.shashankreddy@gmail.com>
2025-11-16 20:37:20 +0530

    Moving from sequential to parellel data fetching for editorial and startup data rendering

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -1,6 +1,6 @@
 import { formatDate } from '@/lib/utils';
 import { client } from '@/sanity/lib/client';
-import { STARTUP_BY_ID_QUERY } from '@/sanity/lib/queries';
+import { PLAYLIST_BY_SLUG_QUERY, STARTUP_BY_ID_QUERY } from '@/sanity/lib/queries';
 import Link from 'next/link';
 import { notFound } from 'next/navigation';
 import React, { Suspense } from 'react'
@@ -15,7 +15,21 @@ const md = markdownit();
 
 const page = async ({params} : {params: Promise<{id:string}>}) => {
     const id = (await params).id;
-    const post  = await client.fetch(STARTUP_BY_ID_QUERY, {id});
+    //This is part of Parallel fetching editor picks as Startup Id query doesn't depend on it
+    const [post, { select: editorPosts }] = await Promise.all([
+    client.fetch(STARTUP_BY_ID_QUERY, { id }),
+    client.fetch(PLAYLIST_BY_SLUG_QUERY, {
+      slug: "editor-picks-new",
+    }),
+  ]);
+
+    //This is part of sequestial rendering
+    // const post  = await client.fetch(STARTUP_BY_ID_QUERY, {id});
+    // const { editorPosts} = await client.fetch(
+    //   PLAYLIST_BY_SLUG_QUERY, {
+    //     slug: "editor-picks-new",
+    //   }
+    // );
     if(!post) return notFound();
     const parsedContent = md.render(post?.pitch || "");
   return (
@@ -66,7 +80,7 @@ const page = async ({params} : {params: Promise<{id:string}>}) => {
           </div>
         </div>
         <hr className="divider" />
-        {/* {editorPosts?.length > 0 && (
+        {editorPosts?.length > 0 && (
           <div className="max-w-4xl mx-auto">
             <p className="text-30-semibold">Editor Picks</p>
 
@@ -76,7 +90,7 @@ const page = async ({params} : {params: Promise<{id:string}>}) => {
               ))}
             </ul>
           </div>
-        )} */}
+        )}
         <Suspense fallback={<Skeleton className="view_skeleton" />}>
           <View id={id} />
         </Suspense> {/* This is Partial Pre rendering */} 
